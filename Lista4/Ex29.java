e36ee82b394aa54fb31f41a8c8ae3c436fe00ee5
JosueFernandes7 <josue7mf@hotmail.com>
2022-04-09 19:54:17 -0300

    ex29

---
+++ Lista4/Ex29.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ex29 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int i=1,j=0,k=0;
+        int produto =1;
+        int contline =0;
+        // i casa centena
+        // j dezena
+        // k unidade
+       while(i<=9) {
+            j=0;
+            while(j<=9){
+                k=0;
+                produto = 1;
+                while(k<=9){
+                    produto = i*j*k;
+                    System.out.print(i+""+j+""+k+" ("+i+"*"+j+"*"+k+"*)");
+                    System.out.println(" = "+produto);
+                    contline++;
+                    if(contline % 20 == 0){
+                        System.out.println("(...)");
+                        System.out.print("Para Continuar Aperte Qualquer tecla: ");
+                        in.next();
+                    }
+                    k++;
+                }
+                j++;
+            }
+            i++;
+        }
+        
+    }
+}
